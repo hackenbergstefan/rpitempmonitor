17bb2443ba6d1425bae0cbe3d6036c0f7e002cc9
Stefan Hackenberg <mail@stefan-hackenberg.de>
2018-05-27 17:05:03 +0200

    Add first example of reading temperature.

--- rpitempmonitor/app.py
+++ rpitempmonitor/app.py
@@ -3,14 +3,23 @@
 
 
 from flask import Flask
+from MAX31865.max31865 import MAX31865
 
 app = Flask('rpitempmonitor')
+
+max31865 = MAX31865(
+    cs_pin=21,
+    miso_pin=22,
+    mosi_pin=23,
+    clk_pin=24,
+)
 
 
 @app.route('/')
 def index():
-    return 'hello'
+    return 'Temperature: %0.2f °C' % max31865.temperature()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    with max31865:
+        app.run(host='0.0.0.0')
