6d07619761a4959535e55bebabccb2ef8d4b512d
Stefan Hackenberg <mail@stefan-hackenberg.de>
2018-05-29 09:26:56 +0200

    Add DataLogger and use it in app.

--- rpitempmonitor/app.py
+++ rpitempmonitor/app.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
+import time
 from flask import Flask, jsonify, render_template
+from datalogger import DataLogger
 try:
     from MAX31865.max31865 import MAX31865
 except ModuleNotFoundError:
     from max31865mock import MAX31865
 
 app = Flask('rpitempmonitor')
+"""Flask-App reflecting this website."""
 
 max31865 = MAX31865(
     cs_pin=21,
@@ -16,16 +19,24 @@ max31865 = MAX31865(
     mosi_pin=23,
     clk_pin=24,
 )
+"""Interface to MAX31865."""
+
+datalogger = None
+"""DataLogger to save logged temperature to file."""
 
 
 @app.route('/')
 def index():
+    global datalogger
+    datalogger = DataLogger('data_%d.csv' % int(time.time()))
     return render_template('index.html')
 
 
 @app.route('/temperature')
 def temperature():
-    return jsonify(temperature=max31865.temperature())
+    temp = max31865.temperature()
+    datalogger.add_data((time.time(), temp))
+    return jsonify(temperature=temp)
 
 
 if __name__ == '__main__':
---
+++ rpitempmonitor/datalogger.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import csv
+import os
+
+
+class DataLogger(object):
+
+    def __init__(self, logger_file, write_interval=10):
+        self.logger_file = logger_file
+        self.write_interval = write_interval
+        self.data = []
+
+    def add_data(self, point):
+        """
+        Add point to data.
+        """
+        self.data.append(point)
+        if len(self.data) % self.write_interval:
+            self.save()
+
+    def save(self):
+        """
+        Save data to file.
+        """
+        getattr(self, 'save_%s' % os.path.splitext(self.logger_file)[1][1:])()
+
+    def save_csv(self):
+        """
+        Save data to csv file.
+        """
+        with open(self.logger_file, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(self.data)
